7a6bb0cafce1d61639fe758e3c08891c1be533b7
Thaina Biudes <thaina.biudes@gmail.com>
2022-06-04 00:16:17 -0300

    feat: adiciona migrations

---
+++ Migrations/20220529210234_AddInvoiceDataTableOnDB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InvoiceAPI.Migrations
+{
+    public partial class AddInvoiceDataTableOnDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "invoice",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    value = table.Column<double>(type: "double", nullable: false),
+                    customer_id = table.Column<long>(type: "bigint", nullable: false),
+                    initial_date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    final_date = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_invoice", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "invoice");
+        }
+    }
+}
