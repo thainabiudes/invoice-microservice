d04a257174eda107f728f0c24a28272c58ec9288
Thaina Biudes <thaina.biudes@gmail.com>
2022-06-04 00:15:18 -0300

    feat: cria configuração de automapper

---
+++ Config/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Invoice.API.Data.ValueObjects;
+using Invoice.API.Model;
+
+namespace Invoice.API.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<InvoiceVO, InvoiceEntity>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
