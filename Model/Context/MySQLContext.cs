a5acbb50a4455fe4d3636b55a61d69d577975d52
Thaina Biudes <thaina.biudes@gmail.com>
2022-06-04 00:14:30 -0300

    feat: cria model

---
+++ Model/Context/MySQLContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Invoice.API.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext() {}
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
+
+        public DbSet<InvoiceEntity> Invoice { get; set; }
+    }
+}
---
+++ Model/InvoiceEntity.cs
@@ -0,0 +1,28 @@
+﻿using Invoice.API.Model.Base;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Invoice.API.Model
+{
+    [Table("invoice")]
+    public class InvoiceEntity : BaseEntity
+    {
+        [Column("value")]
+        [Required]
+        public double Value { get; set; }
+
+        [Column("customer_id")]
+        [Required]
+        public long CustomerId { get; set; }
+
+        [Column("initial_date")]
+        [Required]
+        public DateTime InitialDate { get; set; }
+
+        [Column("final_date")]
+        [Required]
+        public DateTime FinalDate { get; set; }
+
+    }
+}
